b36902e70a2002cc94544860c41e09435548e7ee
t5i0n <tesfayetsion270@gmail.com>
2026-07-08 10:59:14 +0300

    added 404 fallback route

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,24 @@
 import { Route, Routes } from "react-router-dom";
 import AboutPage from "./routes/about";
+import BlogPage from "./routes/blog";
 import ContactPage from "./routes/contact";
-import ServicesPage from "./routes/services";
+import HomePage from "./routes/index";
+import NotFoundPage from "./routes/not-found";
 import OriginsPage from "./routes/origins";
 import ProcessPage from "./routes/process";
-import BlogPage from "./routes/blog";
-import HomePage from "./routes/index";
+import ServicesPage from "./routes/services";
 
 export default function App() {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
       <Route path="/about" element={<AboutPage />} />
-      <Route path="/contact" element={<ContactPage />} />
       <Route path="/services" element={<ServicesPage />} />
       <Route path="/origins" element={<OriginsPage />} />
       <Route path="/process" element={<ProcessPage />} />
       <Route path="/blog" element={<BlogPage />} />
+      <Route path="/contact" element={<ContactPage />} />
+      <Route path="*" element={<NotFoundPage />} />
     </Routes>
   );
 }
