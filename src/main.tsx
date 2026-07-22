9b267229403bff490c0eea865313fc8bf61e0cfb
t5i0n <tesfayetsion270@gmail.com>
2026-07-22 16:41:03 +0300

    add comment

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
 
+// Render the React application and enable client-side routing
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
